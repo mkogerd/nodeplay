21d99ebb05bf0e060bb0041ad38801bab5e72988
Koger Darden <mkogerd@gmail.com>
2018-06-24 18:34:13 -0500

    Make simple networking work

--- iochat/server.js
+++ iochat/server.js
@@ -2,8 +2,15 @@ var app = require('express')();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 
-users = [];
+players = [];
 connections = [];
+
+function Player(id, x, y, color) {
+	this.id = id;
+	this.x = x;
+	this.y = y;
+	this.color = color;
+}
 
 console.log('Server running...');
 
@@ -12,19 +19,38 @@ app.get('/', (req, res) => {
 });
 
 io.sockets.on('connection', (socket) => {
-	connections.push(socket);
-	console.log(`Connected: ${connections.length} sockets connected`);
-	io.emit('initialize', connections.indexOf(socket)); // sending initializing index at the moment
+	var player = new Player(socket.id, 0, 0, '#ff0000');
+	players.push(player);
+	console.log(`New player connected. ID: ${player.id}`);
+
+	console.log(`Connected: ${players.length} sockets connected`);
+	socket.emit('initialize', players); // Send list of all players to populate new client session 
+	socket.broadcast.emit('new player', player); // Send new player data to all existing clients 
 
 	socket.on('send message', (data)=>{
-		console.log(`Connection ${connections.indexOf(socket)}: ${data}`);
+		console.log(`Connection ${players.indexOf(socket)}: ${data}`);
 		io.emit('new message', {msg: data});
+	});
+	
+	socket.on('input', (control)=>{
+		console.log(`id:${player.id}, x:${player.x}, y:${player.y}`);
+		let speed = 5;
+		if(control.left) player.x = player.x - speed;
+		if(control.right) player.x = player.x + speed;
+		if(control.up) player.y = player.y - speed;
+		if(control.down) player.y = player.y + speed;
 	});
 
 	// Disconnect
 	socket.on('disconnect', (data) => {
-		connections.splice(connections.indexOf(socket), 1);
-		console.log(`Disconnected: ${connections.length} sockets connected`);
+		players.splice(players.indexOf(player), 1);
+		console.log(`Disconnected: ${players.length} sockets connected`);
+		io.emit('disconnect', player.id);
 	});
 });
+
+setInterval(()=>{
+	console.log('tick');
+	io.emit('update', players);
+}, 100);
 server.listen(process.env.PORT || 3000);
